61a8bdef11bed118f540520860b619b606538f36
Toby Dylan Hocking <tdhock5@gmail.com>
2018-11-20 12:09:25 -0700

    remove Remotes

--- build.sh
+++ build.sh
@@ -3,6 +3,7 @@ cd ..
 set -o errexit
 rm -rf slurm-release
 cp -r slurm slurm-release
+grep -v Remotes slurm/DESCRIPTION > slurm-release/DESCRIPTION
 PKG_TGZ=$(R CMD build slurm-release|grep building|sed 's/.*‘//'|sed 's/’.*//')
 R CMD INSTALL $PKG_TGZ
 R CMD check --as-cran $PKG_TGZ
