59f36f2eef2a62a0cc19f6806ef85ef569d4f465
Toby Dylan Hocking <toby.hocking@nau.edu>
2019-10-03 15:48:50 -0700

    test fails

--- tests/testthat/test-sacct.R
+++ tests/testthat/test-sacct.R
@@ -2,6 +2,14 @@ library(slurm)
 library(testthat)
 context("sacct")
 
+slurm.txt <- system.file(
+  "extdata", "slurm-unrecognized-unit.txt", package="slurm", mustWork=TRUE)
+test_that("sacct_fread works for prev unrecognized unit", {
+  dt <- sacct_fread(slurm.txt)
+  expect_is(dt, "data.table")
+})
+
+
 test_that("job ID field parsed correctly", {
   sacct.csv.gz <- system.file(
     "data", "sacct-multitype.csv.gz", package="slurm", mustWork=TRUE)
