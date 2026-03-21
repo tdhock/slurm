9bbefb1aa294872154ebe0e555c3c9ddaf62365f
Toby Dylan Hocking <toby.hocking@r-project.org>
2026-03-20 21:53:55 -0400

    link

--- R/sacct.R
+++ R/sacct.R
@@ -40,10 +40,10 @@ sacct_fields <- function
 ### command to run and capture output, default "sacct -e" to query
 ### slurm, can be changed to cat a file for testing.
 ){
-  sacct_lines <- system(cmd, intern=TRUE)
-  match_dt <- nc::capture_all_str(sacct_lines, field="\\w+")
+  slines <- system(cmd, intern=TRUE)
+  match_dt <- nc::capture_all_str(slines, field="\\w+")
   match_dt[["field"]]
-### character vector, items that can be used as format.fields argument to sacct_lines().
+### character vector, items that can be used as format.fields argument to sacct_lines
 }
 
 ### Run sacct_lines then sacct_tasks.
